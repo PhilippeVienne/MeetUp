f5f76d2719ac0a47afaaa7352419fc427b8d644b
Philippe VIENNE <PhilippeGeek@gmail.com>
2013-02-24 22:30:25 +0100

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MeetUp::Application.config.secret_token = 'faf6e62b2c9f92b9e304203decc8679308456061ef37dcce8a62139285cc53cec27ea8574294732511c0f6f38ea7057cc25b58e150bebd9837050be76fe15e5a'
